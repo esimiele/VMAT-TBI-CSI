dd7bb1e66e2e2ec758d5059d18938b661439d97f
Eric Simiele <simiele77@gmail.com>
2023-05-29 11:01:34 -0700

    update all versions to 1.5

--- VMATTBICSIAutoPlanMT/VMATTBICSIOptLoopMT/Properties/AssemblyInfo.cs
+++ VMATTBICSIAutoPlanMT/VMATTBICSIOptLoopMT/Properties/AssemblyInfo.cs
@@ -49,7 +49,7 @@ using VMS.TPS.Common.Model.API;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.4.*")]
+[assembly: AssemblyVersion("1.5.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyInformationalVersion("1.0")]
 
