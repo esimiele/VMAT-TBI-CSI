56b301d0e76017d2b06ebdf8d4feafdc49fb897d
Eric Simiele <simiele77@gmail.com>
2023-08-14 08:11:51 -0700

    minor fix to TBI plan templates

--- VMATTBICSIAutoPlanMT/VMATTBICSIAutoplanningHelpers/helpers/StructureTuningHelper.cs
+++ VMATTBICSIAutoPlanMT/VMATTBICSIAutoplanningHelpers/helpers/StructureTuningHelper.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using VMS.TPS.Common.Model.API;
-using VMS.TPS.Common.Model.Types;
 using TSManipulationType = VMATTBICSIAutoPlanningHelpers.Enums.TSManipulationType;
 
 namespace VMATTBICSIAutoPlanningHelpers.Helpers
