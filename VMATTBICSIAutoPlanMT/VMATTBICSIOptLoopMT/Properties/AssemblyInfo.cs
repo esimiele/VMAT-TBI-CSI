fb4eac585d43e5fa6d2f0fe3271de631dea3875e
Eric Simiele <simiele77@gmail.com>
2024-09-15 16:25:06 -0500

    update versioning and update readme. Ready for push

--- VMATTBICSIAutoPlanMT/VMATTBICSIOptLoopMT/Properties/AssemblyInfo.cs
+++ VMATTBICSIAutoPlanMT/VMATTBICSIOptLoopMT/Properties/AssemblyInfo.cs
@@ -49,8 +49,8 @@ using VMS.TPS.Common.Model.API;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.*")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: AssemblyInformationalVersion("1.0")]
 
 // TODO: Uncomment the following line if the script requires write access.
